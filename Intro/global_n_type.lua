4855077c72c0e3b58deefeae3ed1b262a323d576
Jefferson Tafarel <jeffersontafarel.jor@outlook.com>
2026-02-22 15:03:33 -0300

    cascade boolean test

--- Intro/global_n_type.lua
+++ Intro/global_n_type.lua
@@ -1,4 +1,5 @@
 -- nil e false são tratados como false (valores booleanos)
+-- uma variável não definida vai ser tratada como falsa
 b=10
 print(b)
 print(0 and nil) -- primeiro operador verdadeiro → aparece 'nil'
@@ -7,6 +8,14 @@ print(true or "pera")
 print(5 and "beta")
 print(false and 31)
 print(not "gamma") -- false  
+
+x=5
+y=2
+w=3
+z=1
+
+print((x>y) and x or y)
+print((z>w) and w or z)
 
 -- RESULTADOS
 
