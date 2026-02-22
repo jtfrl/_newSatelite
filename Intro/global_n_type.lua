352ebbc24c5fa29390a6fa4f21bb7fd9de1fbca5
Jefferson Tafarel <jeffersontafarel.jor@outlook.com>
2026-02-22 12:32:50 -0300

    new readme (still improving) and testing types

--- Intro/global_n_type.lua
+++ Intro/global_n_type.lua
@@ -1,3 +1,17 @@
---b
+-- nil e false são tratados como false (valores booleanos)
 b=10
 print(b)
+print(0 and nil) -- primeiro operador verdadeiro → aparece 'nil'
+print(false or "banana") -- último valor aparece
+print(true or "pera")
+print(5 and "beta")
+print(false and 31)
+print(not "gamma") -- false  
+
+-- RESULTADOS
+
+-- // and: se o primeiro operador for falso → o 1o. operador aparece
+-- se verdadeiro → aparece o 2o.
+
+-- // or: se o primeiro operador for verdadeiro → o 1o. operador aparece
+-- se falso → aparece o 2o. 
---
+++ README.md
@@ -0,0 +1,7 @@
+#_newSatelite
+
+Repositório de aprendizagem de programação em Lua
+
+## Execução:
+
+Graças à configuração do VS com ```tasks.json```, para compilar os programas .lua, basta utilizar o comando Ctrl+Shift+B
